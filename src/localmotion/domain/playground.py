bd084671ff14e7d64980920fb0612cea97e136a5
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-10-29 20:08:02 +0100

    Playground import

--- src/localmotion/domain/playground.py
+++ src/localmotion/domain/playground.py
@@ -2,12 +2,10 @@ from src.localmotion.domain.address import Address
 
 
 class Playground:
-    def __init__(self, id_: str, title: str, address: Address, type_: str, status: str, website: str) -> None:
-        self.id = id_
-        self.title = title
+    def __init__(self, id_: str, name: str, address: Address, type_: str, status: str, website: str) -> None:
+        self.initiativeId = id_
+        self.name = name
         self.address = address
-        self.type = type_
-        self.status = status
+        self.type = "smokefree"
+        self.status = "finished"
         self.website = website
-
-
--- src/localmotion/handler/community_api.py
+++ src/localmotion/handler/community_api.py
@@ -1,4 +1,6 @@
 import logging
+
+import jsonpickle as jsonpickle
 import requests
 
 from src.localmotion.domain.result import Result
@@ -13,11 +15,15 @@ class CommunityApi:
 
     def create_or_update(self, playground: Playground):
         try:
-            r = requests.post(self.target_endpoint, data=playground)
+            json = jsonpickle.encode(playground, unpicklable=False)
+            r = requests.post(self.target_endpoint, data=json)
             logging.info(r)
-            Result.success(playground, "Added playground {} to Local Motion".format(playground.title))
+            if 200 <= r.status_code < 300:
+                Result.success(playground, "Added playground {} to Local Motion".format(playground.name))
+            else:
+                return Result.failure(playground, "Status {}".format(r.status_code))
         except BaseException as e:
             failure_message = "Could not add playground {} to Local Motion at {}".format(
-                playground.title, self.target_endpoint)
-            return Result.failure(playground, failure_message, e)
+                playground.name, self.target_endpoint)
+            return Result.exception(playground, failure_message, e)
 
